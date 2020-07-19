7becf43b625fe283d8190796a96ab2360bf92f84
unknown <Goy061154>
2020-07-19 15:00:12 +0700

    Updates

---
+++ precache-manifest.fc86852b6d989b5f172a82739314835b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc279132cc7dbce104b19161c36f750",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "de8916be03ea51e0fee3",
+    "url": "/react-web/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df9ee74c2726dcdfd222",
+    "url": "/react-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de8916be03ea51e0fee3",
+    "url": "/react-web/static/js/2.ef9bacb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-web/static/js/2.ef9bacb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9ee74c2726dcdfd222",
+    "url": "/react-web/static/js/main.2cc6f43a.chunk.js"
+  },
+  {
+    "revision": "dc5802d74fec622bbda3",
+    "url": "/react-web/static/js/runtime-main.58bca4b6.js"
+  }
+]);
